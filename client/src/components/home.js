29b9ef6a6750b992ffb76f114867cca50d9e2e48
Marcel <Marcelyap31@gmail.com>
2022-10-22 14:20:52 +0800

    Updated 21/10/2022 1700

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -53,22 +53,19 @@ export default function Home() {
           Hey, you. You're finally awake. You were trying to cross the border, right? Walked right into that Imperial ambush, same as us, and that thief over there. Damn you Stormcloaks. Skyrim was fine until you came along. Empire was nice and lazy. If they hadn't been looking for you, I could've stolen that horse and be halfway to Hammerfell. You there. You and me - we shouldn't be here. It's these Stormcloaks the Empire wants. We're all brothers and sisters in binds now, thief. Shut up back there! And what's wrong with him, huh? Watch your tongue. You're speaking to Ulfric Stormcloak, the true High King. Ulfric? The Jarl of Windhelm? You're the leader of the rebellion. But if they've captured you... Oh gods, where are they taking us? I don't know where we're going, but Sovngarde awaits. No, this can't be happening. This isn't happening. Hey, what village are you from, horse thief? Why do you care? A Nord's last thoughts should be of home. Rorikstead. I'm... I'm from Rorikstead.{" "}
           </div>
         </div>
+        <div className="row row1 row-book-list">
+          <div className="text-animation">
+          In the beginning God created the heavens and the earth.  Now the earth <a href='/records' className="book-list-link"> Book List </a>was formless and empty, darkness was over the surface of the <a href='/records' className="book-list-link"> Book List </a> deep, and the Spirit of God was hovering over the waters. And God said, “Let there be light,” and there was light. 4 God saw that the light was good, and he separated the light from the darkness. 5 God called the light “day,” and the darkness he called “night.” And there was evening, and there was morning—the first day. And God said, “Let there be a vault between the waters to separate water from water.” 7 So God made the vault and separated the water under the vault from the water above it. And it was so. 8 God called the vault “sky.” And there was evening, and there was morning—the second day.
+          </div>
+        </div>
+
+
         <div className="row row2">
           <div className="text-animation">
           According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. {" "}
           </div>
         </div>
-        <div className="row row1 row-book-list">
-          <div className="text-animation">
-          In the beginning God created the heavens and the earth.  Now the earth <a href='/records' className="book-list-link"> Book List </a>was formless and empty, darkness was over the surface of the <a href='/records' className="book-list-link"> Book List </a> deep, and the Spirit of God was hovering over the waters. And God said, “Let there be light,” and there was light. 4 God saw that the light was good, and he separated the light from the darkness. 5 God called the light “day,” and the darkness he called “night.” And there was evening, and there was morning—the first day. And God said, “Let there be a vault between the waters to separate water from water.” 7 So God made the vault and separated the water under the vault from the water above it. And it was so. 8 God called the vault “sky.” And there was evening, and there was morning—the second day.
-            <a href="/records" className="book-list-link">
-              {" "}
-              Book List{" "}
-            </a>{" "}
-            I shall cross the River myself one day.' To which the other half of
-            his mind always replied: 'Not yet.'{" "}
-          </div>
-        </div>
+        
         <div className="row row3">
           <div className="text-animation">
           Why, Mr. Anderson, why? Why, why do you do it? Why, why get up? Why keep fighting? Do you believe you’re fighting for something, for more than your survival? Can you tell me what it is, do you even know? Is it freedom or truth, perhaps peace – could it be for love? Illusions, Mr. Anderson, vagaries of perception. Temporary constructs of a feeble human intellect trying desperately to justify an existence that is without meaning or purpose. And all of them as artificial as the Matrix itself. Although, only a human mind could invent something as insipid as love. You must be able to see it, Mr. Anderson, you must know it by now! You can’t win, it’s pointless to keep fighting! Why, Mr. Anderson, why, why do you persist?{" "}
